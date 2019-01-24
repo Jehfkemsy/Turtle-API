65179cfa1fd1db7195558fcd4ca22fc97309db26
Carlos Lahrssen <carloslahrssen@Carloss-MBP.hsd1.fl.comcast.net>
2019-01-24 00:27:47 -0500

    prettier

--- src/controllers/candidate.js
+++ src/controllers/candidate.js
@@ -4,26 +4,31 @@ import logger from "../utils/logger";
 import httpResponse from "../utils/httpResponses";
 
 import Candidate from "../models/candidate";
-import Applicant from '../models/applicant';
-
+import Applicant from "../models/applicant";
 
 const create = async (req, res) => {
-		let { hacker, company } = req.body;
-	
-		const applicant = await Applicant.findOne({email: hacker.email});
-		const { firstName, lastName, email, levelOfStudy, resume } = applicant;
+  let { hacker, company } = req.body;
 
-	try{
-		const candidateObj = {firstName, lastName, email, levelOfStudy, resume, company};
-		await applicationService.validateCandidate(candidateObj);
-		const candidate = await Candidate.create();
-	
-		httpResponse.successResponse(res, candidate);
+  const applicant = await Applicant.findOne({ email: hacker.email });
+  const { firstName, lastName, email, levelOfStudy, resume } = applicant;
 
-	} catch(e) {
-		logger.info({ e, application: "Candidate" });
-		httpResponse.failureResponse(res, e);
-	}
+  try {
+    const candidateObj = {
+      firstName,
+      lastName,
+      email,
+      levelOfStudy,
+      resume,
+      company
+    };
+    await applicationService.validateCandidate(candidateObj);
+    const candidate = await Candidate.create();
+
+    httpResponse.successResponse(res, candidate);
+  } catch (e) {
+    logger.info({ e, application: "Candidate" });
+    httpResponse.failureResponse(res, e);
+  }
 };
 const read = (req, res) => {};
 
