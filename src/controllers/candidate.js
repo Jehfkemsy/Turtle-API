fb0260f63a2d78cc1920cb3c615455f36db7c98e
Carlos Lahrssen <carloslahrssen@Carloss-MBP.hsd1.fl.comcast.net>
2019-01-24 00:31:54 -0500

    fix validation

--- src/controllers/candidate.js
+++ src/controllers/candidate.js
@@ -8,11 +8,11 @@ import Applicant from "../models/applicant";
 
 const create = async (req, res) => {
   let { hacker, company } = req.body;
+	try {	
+		await applicationService.validateCandidate(hacker);
+		const applicant = await Applicant.findOne({ email: hacker.email });
+		const { firstName, lastName, email, levelOfStudy, resume } = applicant;
 
-  const applicant = await Applicant.findOne({ email: hacker.email });
-  const { firstName, lastName, email, levelOfStudy, resume } = applicant;
-
-  try {
     const candidateObj = {
       firstName,
       lastName,
@@ -21,7 +21,6 @@ const create = async (req, res) => {
       resume,
       company
     };
-    await applicationService.validateCandidate(candidateObj);
     const candidate = await Candidate.create(candidateObj);
 
     httpResponse.successResponse(res, candidate);
