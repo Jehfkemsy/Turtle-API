4335f813cea97333302eee7eabc6fb8a975ed420
Carlos Lahrssen <carloslahrssen@Carloss-MBP.hsd1.fl.comcast.net>
2019-01-24 00:26:39 -0500

    make candidate obj

--- src/controllers/candidate.js
+++ src/controllers/candidate.js
@@ -1,6 +1,4 @@
 import applicationService from "../services/application";
-import drive from "../services/google/drive";
-import sheets from "../services/google/sheets";
 
 import logger from "../utils/logger";
 import httpResponse from "../utils/httpResponses";
@@ -8,18 +6,18 @@ import httpResponse from "../utils/httpResponses";
 import Candidate from "../models/candidate";
 import Applicant from '../models/applicant';
 
-const { CARNIVAL_FOLDER_ID, EXPRESS_FOLDER_ID, GE_FOLDER_ID } = process.env;
 
 const create = async (req, res) => {
 		let { hacker, company } = req.body;
 	
 		const applicant = await Applicant.findOne({email: hacker.email});
 		const { firstName, lastName, email, levelOfStudy, resume } = applicant;
-		
+
 	try{
-		const candidate = await Candidate.create({firstName, lastName, email, levelOfStudy, resume, company});
+		const candidateObj = {firstName, lastName, email, levelOfStudy, resume, company};
+		await applicationService.validateCandidate(candidateObj);
+		const candidate = await Candidate.create();
 	
-		await applicationService.validateCandidate(candidate);
 		httpResponse.successResponse(res, candidate);
 
 	} catch(e) {
