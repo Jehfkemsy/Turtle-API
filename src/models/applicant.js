178c56deec6778989a88779fa4badb2895f42f4b
Tommy <carrascalx@gmail.com>
2019-03-27 16:34:00 -0400

    updated models with some missing info

--- src/models/applicant.js
+++ src/models/applicant.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 const applicantSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
+  email: {type: String, required: true},
+  password: {type:String, required: true},
+  schoolName: {type:String, required: true},
   levelOfStudy: { type: String, default: "N/A" },
   graduationYear: { type: String, required: true },
   major: { type: String, required: true },
