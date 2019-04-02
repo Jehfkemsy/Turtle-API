f4f733e39f0839ac253497d54c772e4957a353cc
Tommy <carrascalx@gmail.com>
2019-04-02 17:31:42 -0400

    changed db

--- src/db/index.js
+++ src/db/index.js
@@ -1,54 +1,27 @@
-// import mongoose from "mongoose";
-
-// const { MONGO_USER, MONGO_PASSWORD, MONGO_URI } = process.env;
-
-// const options = {
-//   user: MONGO_USER,
-//   pass: MONGO_PASSWORD,
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   dbName: "mango2019",
-//   useFindAndModify: false
-// };
-
-// const db = () =>
-//   Promise.resolve(
-//     mongoose.connect(
-//       MONGO_URI,
-//       options
-//     )
-//   );
-
-// db()
-//   .then(() => console.log("> 🗄  Mongo connected"))
-//   .catch(e => console.log("> Mongo error ", e.message));
-
-//FOR TESTING ONLY 
-
 import mongoose from "mongoose";
 
 const { MONGO_USER, MONGO_PASSWORD, MONGO_URI } = process.env;
 
-// const options = {
-//   // user: MONGO_USER,
-//   // pass: MONGO_PASSWORD,
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   dbName: "passport-tutorial",
-//   useFindAndModify: false
-// };
-
-const server = 'localhost:27017';
-const database = 'passport-tutorial'
+const options = {
+  user: MONGO_USER,
+  pass: MONGO_PASSWORD,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  dbName: "mango2019",
+  useFindAndModify: false
+};
 
 const db = () =>
   Promise.resolve(
     mongoose.connect(
-      `mongodb://${server}/${database}`
+      MONGO_URI,
+      options
     )
   );
 
 db()
   .then(() => console.log("> 🗄  Mongo connected"))
   .catch(e => console.log("> Mongo error ", e.message));
+
+
 
