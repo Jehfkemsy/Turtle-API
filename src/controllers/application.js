29c398479c13a7054a57c957aacbfcce37030a98
Tommy <carrascalx@gmail.com>
2019-04-02 16:04:56 -0400

    adding apply route

--- src/controllers/application.js
+++ src/controllers/application.js
@@ -12,16 +12,8 @@ import Applicant from "../models/applicant";
 const { GOOGLE_FOLDER_ID, GOOGLE_SPREADSHEET_ID } = process.env;
 
 const create = async (req, res) => {
-  fileService.extractResume(req, res, async err => {
-    if (err) return httpResponse.failureResponse(res, err);
-    const { file } = req;
 
-    const{firstName,lastName,email,password,schoolName,levelOfStudy,
-          graduationYear,major,gender,dob,race,phoneNumber,shirtSize,
-          dietaryRestriction,firstTimeHack,howDidYouHear,
-          favoriteEvents,areaOfFocus,resume,linkedIn,portfolio,github,
-          reasonForAttending,haveBeenToShell,likeAMentor,
-          needReimburesment,location,mlh,fiu,shellHacks} = req.body;
+    const{firstName,lastName,email,password} = req.body;
           
     
     //need to generate avatarID, ShellID, and Hash password
@@ -30,54 +22,18 @@ const create = async (req, res) => {
       lastName,
       email,
       password,
-      schoolName,
-      levelOfStudy,
-      graduationYear,
-      major,
-      gender,
-      dob,
-      race,
-      phoneNumber,
-      shirtSize,
       avatarID:"Id1",
-      dietaryRestriction,
-      firstTimeHack,
-      howDidYouHear,
-      favoriteEvents,
-      areaOfFocus,
-      resume,
-      linkedIn,
-      portfolio,
-      github,
-      reasonForAttending,
-      haveBeenToShell,
-      likeAMentor,
       applicationStatus: 'not applied',
-      needReimburesment,
-      location,
       shellID: 'wewe',
-      shirtSize,
     };
 
     try {
-      if (!file) throw "Resume is required.";
 
       /**
        * Validate applicant fields
        */
-      await applicationService.validateHacker(fields);
+      // await applicationService.validateHacker(fields);
 
-      /**
-       * Upload resume to google drive
-       */
-      const filename = fields.email.match(/.*?(?=@|$)/i)[0];
-
-      fields.resume = "N/A";
-
-      if (GOOGLE_FOLDER_ID) {
-        const resumeUrl = await drive.upload(file, filename, GOOGLE_FOLDER_ID);
-        fields.resume = resumeUrl;
-      }
 
       /**
        * Insert applicant in the database
@@ -87,19 +43,19 @@ const create = async (req, res) => {
       /**
        * Send applicant email
        */
-      mailService.applied(fields);
+      // mailService.applied(fields);
 
       /**
        * Insert applicant in google sheets
        */
-      sheets.write("Applicants", fields);
+      // sheets.write("Applicants", fields);
 
       httpResponse.successResponse(res, applicant);
     } catch (e) {
       logger.info({ e, application: "Hacker", email: fields.email });
       httpResponse.failureResponse(res, e);
     }
-  });
+  
 };
 
 const read = async (req, res) => {
@@ -244,6 +200,95 @@ const confirm = async (req,res) => {
   catch(e){
     httpResponse.failureResponse(res, e);
   }
+}
+
+const apply = async (req,res) => {
+  fileService.extractResume(req, res, async err => {
+    if (err) return httpResponse.failureResponse(res, err);
+    const { file } = req;
+
+    const{email,schoolName,levelOfStudy,
+          graduationYear,major,gender,dob,race,phoneNumber,shirtSize,
+          dietaryRestriction,firstTimeHack,howDidYouHear,
+          favoriteEvents,areaOfFocus,resume,linkedIn,portfolio,github,
+          reasonForAttending,haveBeenToShell,likeAMentor,
+          needReimburesment,location} = req.body;
+          
+    
+    //need to generate avatarID, ShellID, and Hash password
+    const fields = {
+      schoolName,
+      levelOfStudy,
+      graduationYear,
+      major,
+      gender,
+      dob,
+      race,
+      phoneNumber,
+      shirtSize,
+      dietaryRestriction,
+      firstTimeHack,
+      howDidYouHear,
+      favoriteEvents,
+      areaOfFocus,
+      resume,
+      linkedIn,
+      portfolio,
+      github,
+      reasonForAttending,
+      haveBeenToShell,
+      likeAMentor,
+      applicationStatus: 'applied',
+      needReimburesment,
+      location,
+      shirtSize,
+    };
+
+    try {
+      if (!file) throw "Resume is required.";
+
+      /**
+       * Validate applicant fields
+       */
+      // await applicationService.validateHacker(fields);
+
+      /**
+       * Upload resume to google drive
+       */
+      // const filename = fields.email.match(/.*?(?=@|$)/i)[0];
+
+      // fields.resume = "N/A";
+
+      // if (GOOGLE_FOLDER_ID) {
+      //   const resumeUrl = await drive.upload(file, filename, GOOGLE_FOLDER_ID);
+      //   fields.resume = resumeUrl;
+      // }
+
+      /**
+       * update applicant in the database
+       */
+      const user = await Applicant.findOneAndUpdate(
+        {email},
+        {fields}
+        ).exec();
+        return httpResponse.successResponse(res,null)
+
+      /**
+       * Send applicant email
+       */
+      mailService.applied(fields);
+
+      /**
+       * Insert applicant in google sheets
+       */
+      sheets.write("Applicants", fields);
+
+      httpResponse.successResponse(res, applicant);
+    } catch (e) {
+      logger.info({ e, application: "Hacker", email: fields.email });
+      httpResponse.failureResponse(res, e);
+    }
+  });
 }
 
 export default { create, read, update,confirm, acceptOne, acceptSchool};
