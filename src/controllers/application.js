c93bb0bae801096060d848e2c1bf70fbdcfa627d
Tommy Carrascal (Intern) <tcarrascal@lmtc-tcarrascal.jomax.paholdings.com>
2019-05-30 10:40:06 -0700

    adding missing email confirm function

--- src/controllers/application.js
+++ src/controllers/application.js
@@ -92,8 +92,8 @@ const create = async (req, res) => {
       /**
        * Validate applicant fields
        */
-    
-      await applicationService.validateHacker(email);
+      //await applicationService.validateHacker(fields);
+
 
       /**
        * Insert applicant in the database
@@ -486,33 +486,44 @@ const resetPassword = async (req,res) => {
       httpResponse.failureResponse(res,err);
     }
 }
-const remindConfirm = async (req, res) =>
-{
+
+const confirmEmail = (req,res) => {
+  const email = req.params.email;
+
   try{
+    const token = req.params.token;
 
-        const remind = await Applicant.find({applicationStatus : "accepted"})
+    const applicant = await Applicant.findOne({email})
 
-        remind.map(applicant => {mailService.applied(applicant)})
-}catch(e)
-{
-  logger.info({ e, application: "Hacker", email: email });
-  httpResponse.failureResponse(res, e)
-}
-}
+    if(!email){ throw "Email not found"}
 
-const remindApply = async (req,res) =>
-{
-  try{
-    const remind = await Applicant.find({applicationStatus : "not applied"})
-        remind.map(applicant => {mailService.applied(applicant)})
+    if(applicant.emailConfirmed){
+      httpResponse.successResponse(res,"Email already confirmed");
+    }
 
-  httpResponse.successResponse(res);
-  }catch(e)
-  {
-    logger.info({ e});
-    httpResponse.failureResponse(res, e)
+    if(applicant.emailConfirmationToken != token){
+      throw "Email confirmation link is invalid"
+    }
+
+    await Applicant.findOneAndUpdate({email},{emailConfirmed: true});
+    const confirmedApplicant = await Applicant.findOne({email});
+
+    if(!confirmedApplicant){
+      throw "Email not confirmed, please try again later"
+    }
+
+    if(!confirmedApplicant.emailConfirmed){
+      throw "Email not confirmed, please try again later 2"
+    }
+
+    httpResponse.successResponse(res,"Email succesfully confirmed");
   }
+  catch(err){
+    console.log(err);
+    httpResponse.failureResponse(res,err);
+  }
+  
 }
 
+export default { create, read, readOne, update, confirm, acceptOne, acceptSchool, apply, unconfirm, login, forgotPassword, resetPassword, checkIn, accept, confirmEmail };
 
-export default { create, read, readOne, update, confirm, apply, unconfirm, login, forgotPassword,resetPassword, checkIn, accept, remindConfirm, remindApply};
