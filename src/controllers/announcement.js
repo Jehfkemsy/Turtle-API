22d1ad498f8d76e53144cd8cb7240b26847ecd18
Maurice Barnes <mauriceb2197@gmail.com>
2019-07-25 17:38:07 -0400

    got rid of commented out code

--- src/controllers/announcement.js
+++ src/controllers/announcement.js
@@ -5,41 +5,6 @@ import httpResponse from "../utils/httpResponses";
 import moment from 'moment';
 import logger from "../utils/logger";
 
-// const {CHANNEL_ID} = process.env;
-
-// const create = async (req, res) => {
-//   const { challenge, event } = req.body;
-//   if(!challenge && event && event.channel === CHANNEL_ID) {
-//     const verify = await Announcement.find({message_id: event.client_msg_id});
-//     if(verify.client_msg_id !== event.client_msg_id) {
-//       try{
-//         const time = moment().tz('America/New_York').format('h:mma');
-//         const announce = await Announcement.create({ message_id: event.client_msg_id, message: event.text, time });
-//         req.io.emit('announcement', announce);
-//       } catch( e) {
-//         httpResponse.failureResponse(res, { e });
-//       }
-//     } 
-//   } else {
-//     console.log(event.channel, event.text);
-//   }
-//   if(challenge)
-//   {
-//     httpResponse.successResponse(res, { challenge });
-//   }
-    
-// };
-
-// const read = async (req, res) => {
-//   try {
-//     const announcements = await Announcement.find();
-//     console.log({announcements});
-//     httpResponse.successResponse(res, announcements);
-//   } catch (e) {
-//     httpResponse.failureResponse(res, e);
-//   }
-// };
-
 const create = async(req, res) => 
 {
   try
